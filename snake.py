8129e1d6d241cb14e7bc76ae3824452f20c22e3a
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2020-05-17 12:06:23 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -65,9 +65,9 @@ while running:
     
     # Drawing the food and the snake
     for x in snakeList:
-        pygame.draw.rect(win, (0, 255, 0), (x[0], x[1], 25, 25))
+        pygame.draw.rect(win, (0, 255, 0), (x[0], x[1], 20, 20))
 
-    snake = pygame.draw.rect(win, (0, 255, 0), (snakeX, snakeY, 25, 25))
+    snake = pygame.draw.rect(win, (0, 255, 0), (snakeX, snakeY, 20, 20))
     food = pygame.draw.rect(win, (255, 0, 0), (foodX, foodY, 15, 15))
 
     # Checking if the snake touched the food
